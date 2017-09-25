1c06233f615f3c4301c7a27bd8c77bc312c326f3
Erick Sanchez <esericksanc@gmail.com>
2017-09-24 20:21:55 -0700

    Second persistence store

---
+++ iLogs - Swift/Directory.swift
@@ -0,0 +1,24 @@
+//
+//  Directory.swift
+//  iLogs - Swift
+//
+//  Created by Erick Sanchez on 9/24/17.
+//  Copyright © 2017 Erick Sanchez. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Directory {
+    convenience init(info: DirectoryInfo, parent: Directory?, `in` context: NSManagedObjectContext) {
+        self.init(context: context)
+        
+        self.info = info
+    }
+}
+
+extension NSFetchedResultsController {
+    func directory(at indexPath: IndexPath) -> Directory {
+        return self.object(at: indexPath) as! Directory
+    }
+}
