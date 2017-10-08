26a7233ceeb1b0e25f308c69046a9101e86c4e14
Erick Sanchez <esericksanc@gmail.com>
2017-10-07 17:09:25 -0700

    Convert WeatherCondtionValues to Enum Types

--- iLogs - Swift/Entry+Meta.swift
+++ iLogs - Swift/Entry+Meta.swift
@@ -9,9 +9,13 @@
 import Foundation
 import UIKit
 
-extension Entry {
-    enum WeatherConditionValues {
-        case None
+/** Pass this protocol to allow any of the scales as a single type */
+protocol ScaleDescriptor {
+    var meta: (name: String, rawValue: Int16) { get }
+}
+
+extension WeatherCondition {
+    enum Types {
         case Sunny
         case Clear
         case Cloudy(CloudyScale?)
@@ -22,67 +26,143 @@ extension Entry {
         case Rainy(RainScale?)
         case Stormy(StormScale?)
         case Snowy(SnowyScale?)
-        
-        enum CloudyScale {
+        //TODO : Rename Weather Conditions
+        enum CloudyScale: ScaleDescriptor {
             case Light
             case Broken
             case Overcast
+            
+            static var array: [CloudyScale] = [.Light,.Broken,.Overcast]
+            
+            var meta: (name: String, rawValue: Int16) {
+                switch self {
+                case .Light:
+                    return ("Light Clouds",1)
+                case .Broken:
+                    return ("Broken Clouds",2)
+                case .Overcast:
+                    return ("Overcast",3)
+                }
+            }
         }
         
-        enum WindyScale {
+        enum WindyScale: ScaleDescriptor {
             case Low
             case Mild
             case Strong
+            
+            static var array: [WindyScale] = [.Low,.Mild,.Strong]
+            
+            var meta: (name: String, rawValue: Int16) {
+                switch self {
+                case .Low:
+                    return ("Low Winds",1)
+                case .Mild:
+                    return ("Mild Winds",2)
+                case .Strong:
+                    return ("Strong Winds",3)
+                }
+            }
         }
         
-        enum FogScale {
+        enum FogScale: ScaleDescriptor {
             case Light
             case Mild
-            case Think
+            case Thick
+            
+            static var array: [FogScale] = [.Light,.Mild,.Thick]
+            
+            var meta: (name: String, rawValue: Int16) {
+                switch self {
+                case .Light:
+                    return ("Light Fog",1)
+                case .Mild:
+                    return ("Mild Fog",2)
+                case .Thick:
+                    return ("Thick Fog",3)
+                }
+            }
         }
         
-        enum RainScale {
+        enum RainScale: ScaleDescriptor {
             case Drizzle
             case Shower
             case Pour
+            
+            static var array: [RainScale] = [.Drizzle,.Shower,.Pour]
+            
+            var meta: (name: String, rawValue: Int16) {
+                switch self {
+                case .Drizzle:
+                    return ("Drizzle",1)
+                case .Shower:
+                    return ("Showers",2)
+                case .Pour:
+                    return ("Pouring",3)
+                }
+            }
         }
         
-        enum StormScale {
+        enum StormScale: ScaleDescriptor {
             case Mild
             case Heavy
             case Lighting
+            
+            static var array: [StormScale] = [.Mild,.Heavy,.Lighting]
+            
+            var meta: (name: String, rawValue: Int16) {
+                switch self {
+                case .Mild:
+                    return ("Mild Storm",1)
+                case .Heavy:
+                    return ("Heavy Storm",2)
+                case .Lighting:
+                    return ("Lighting Storm",3)
+                }
+            }
         }
         
-        enum SnowyScale {
+        enum SnowyScale: ScaleDescriptor {
             case Light
             case Mild
             case Hale
+            
+            static var array: [SnowyScale] = [.Light,.Mild,.Hale]
+            
+            var meta: (name: String, rawValue: Int16) {
+                switch self {
+                case .Light:
+                    return ("Light Snow",1)
+                case .Mild:
+                    return ("Mild Snow",2)
+                case .Hale:
+                    return ("Hale",3)
+                }
+            }
         }
         
-        var meta: (name: String, image: (icon: UIImage, enlarged: UIImage?), scale: String?) {
+        var meta: (name: String, rawValue: Int16, image: (icon: UIImage, enlarged: UIImage?), scale: String?) {
             switch self {
             case .Sunny:
-                return (String(describing: self),(#imageLiteral(resourceName: "misc_weatherSunny"),#imageLiteral(resourceName: "misc_weatherSunny-enlarged")),nil)
+                return (String(describing: self), 1,(#imageLiteral(resourceName: "misc_weatherSunny"),#imageLiteral(resourceName: "misc_weatherSunny-enlarged")),nil)
             case .Clear:
-                return (String(describing: self),(#imageLiteral(resourceName: "misc_weatherClear"),#imageLiteral(resourceName: "misc_weatherClear-enlarged")),nil)
+                return (String(describing: self), 2,(#imageLiteral(resourceName: "misc_weatherClear"),#imageLiteral(resourceName: "misc_weatherClear-enlarged")),nil)
             case .Gloomy:
-                return (String(describing: self),(#imageLiteral(resourceName: "misc_weatherGloomy"),#imageLiteral(resourceName: "misc_weatherGloomy-enlarged")),nil)
+                return (String(describing: self), 3,(#imageLiteral(resourceName: "misc_weatherGloomy"),#imageLiteral(resourceName: "misc_weatherGloomy-enlarged")),nil)
             case . Misty:
-                return (String(describing: self),(#imageLiteral(resourceName: "misc_weatherMisty"),#imageLiteral(resourceName: "misc_weatherMisty-enlarged")),nil)
+                return (String(describing: self), 4,(#imageLiteral(resourceName: "misc_weatherMisty"),#imageLiteral(resourceName: "misc_weatherMisty-enlarged")),nil)
             case .Cloudy(let scale):
-                return ("Cloudy",(#imageLiteral(resourceName: "misc_weatherCloudy"),#imageLiteral(resourceName: "misc_weatherCloudy-enlarged")),String(describing: scale ?? nil))
+                return (scale?.meta.name ?? "Cloudy", 5,(#imageLiteral(resourceName: "misc_weatherCloudy"),#imageLiteral(resourceName: "misc_weatherCloudy-enlarged")),String(describing: scale ?? nil))
             case .Windy(let scale):
-                return ("Windy",(#imageLiteral(resourceName: "misc_weatherWindy"),#imageLiteral(resourceName: "misc_weatherWindy-enlarged")),String(describing: scale ?? nil))
+                return (scale?.meta.name ?? "Windy", 6,(#imageLiteral(resourceName: "misc_weatherWindy"),#imageLiteral(resourceName: "misc_weatherWindy-enlarged")),String(describing: scale ?? nil))
             case .Foggy(let scale):
-                return ("Fog",(#imageLiteral(resourceName: "misc_weatherFoggy"),#imageLiteral(resourceName: "misc_weatherFoggy-enlarged")),String(describing: scale ?? nil))
+                return (scale?.meta.name ?? "Foggy", 7,(#imageLiteral(resourceName: "misc_weatherFoggy"),#imageLiteral(resourceName: "misc_weatherFoggy-enlarged")),String(describing: scale ?? nil))
             case .Rainy(let scale):
-                return ("Rainy",(#imageLiteral(resourceName: "misc_weatherRainy"),#imageLiteral(resourceName: "misc_weatherRainy-enlarged")),String(describing: scale ?? nil))
+                return (scale?.meta.name ?? "Rainy", 8,(#imageLiteral(resourceName: "misc_weatherRainy"),#imageLiteral(resourceName: "misc_weatherRainy-enlarged")),String(describing: scale ?? nil))
             case .Stormy(let scale):
-                return ("Stormy",(#imageLiteral(resourceName: "misc_weatherStormy"),#imageLiteral(resourceName: "misc_weatherStormy-enlarged")),String(describing: scale ?? nil))
+                return (scale?.meta.name ?? "Stormy", 9,(#imageLiteral(resourceName: "misc_weatherStormy"),#imageLiteral(resourceName: "misc_weatherStormy-enlarged")),String(describing: scale ?? nil))
             case .Snowy(let scale):
-                return ("Snowy",(#imageLiteral(resourceName: "misc_weatherSnowy"),#imageLiteral(resourceName: "misc_weatherSnowy-enlarged")),String(describing: scale ?? nil))
-            case .None:
-                return (String(describing: self),(#imageLiteral(resourceName: "misc_weather-disabled"),nil),nil)
+                return (scale?.meta.name ?? "Snowy", 10,(#imageLiteral(resourceName: "misc_weatherSnowy"),#imageLiteral(resourceName: "misc_weatherSnowy-enlarged")),String(describing: scale ?? nil))
             }
         }
         
@@ -90,8 +170,10 @@ extension Entry {
             return self.array.count
         }
         
-        static var array: [WeatherConditionValues] {
-            return [.Sunny,.Clear,.Cloudy(nil),.Windy(nil),.Foggy(nil),.Gloomy,.Misty,.Rainy(nil),.Stormy(nil),.Snowy(nil)]
+        static var array: [Types] = [.Sunny,.Clear,.Cloudy(nil),.Windy(nil),.Foggy(nil),.Gloomy,.Misty,.Rainy(nil),.Stormy(nil),.Snowy(nil)]
+        
+        static func type(`for` value: Int16) -> WeatherCondition.Types? {
+            return WeatherCondition.Types.array.first { $0.meta.rawValue == value }
         }
     }
 }
--- iLogs - Swift/Entry.swift
+++ iLogs - Swift/Entry.swift
@@ -25,3 +25,40 @@ extension NSFetchedResultsController {
         return object(at: indexPath) as! Entry
     }
 }
+
+extension WeatherCondition {
+    convenience init(conditionType: WeatherCondition.Types, scaleType scale: ScaleDescriptor, `in` context: NSManagedObjectContext) {
+        self.init(context: context)
+        
+        self.conditionValue = conditionType.meta.rawValue
+        self.scaleValue = scale.meta.rawValue
+    }
+    
+    /** Convert Weather Condition to a type. This includes the associated values as an optional scale */
+    var conditionType: WeatherCondition.Types {
+        //Looks for the weather condition type according to the condition value, non-nil
+        let type = WeatherCondition.Types.array.first { $0.meta.rawValue == conditionValue }!
+        switch type {
+        case .Cloudy:
+            //Looks for the scale type according to the scale value
+            return .Cloudy(WeatherCondition.Types.CloudyScale.array.first { $0.meta.rawValue == scaleValue })
+        case .Windy:
+            //Looks for the scale type according to the scale value
+            return .Windy(WeatherCondition.Types.WindyScale.array.first { $0.meta.rawValue == scaleValue })
+        case .Foggy:
+            //Looks for the scale type according to the scale value
+            return .Foggy(WeatherCondition.Types.FogScale.array.first { $0.meta.rawValue == scaleValue })
+        case .Rainy:
+            //Looks for the scale type according to the scale value
+            return .Rainy(WeatherCondition.Types.RainScale.array.first { $0.meta.rawValue == scaleValue })
+        case .Stormy:
+            //Looks for the scale type according to the scale value
+            return .Stormy(WeatherCondition.Types.StormScale.array.first { $0.meta.rawValue == scaleValue })
+        case .Snowy:
+            //Looks for the scale type according to the scale value
+            return .Snowy(WeatherCondition.Types.SnowyScale.array.first { $0.meta.rawValue == scaleValue })
+        default:
+            return type
+        }
+    }
+}
